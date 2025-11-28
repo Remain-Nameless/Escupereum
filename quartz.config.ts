1d0c5d1424dd9d8c0f105959004b1d122eb4bb6d
Renmain-Nameless <evita.dragonowliss@mail.ru>
2025-11-28 22:27:34 +0300

    vault backup: 2025-11-28 22:27:34

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "ru-R",
+    locale: "en-EN",
     baseUrl: "remain-nameless.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
