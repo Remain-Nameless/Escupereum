b73ef6371ef8ab6c9e87f3a5e9405192eb40530f
Renmain-Nameless <evita.dragonowliss@mail.ru>
2025-11-28 22:00:12 +0300

    vault backup: 2025-11-28 22:00:12

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Cormorant Unicase",
+        header: "Cormorant Unicase", 35px;
         body: "Jost",
         code: "IBM Plex Mono",
       },
