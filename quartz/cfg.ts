f795783a34dfd00394bc93a83f9bd7a8c5d52bed
Renmain-Nameless <evita.dragonowliss@mail.ru>
2025-12-04 19:57:21 +0300

    vault backup: 2025-12-04 19:57:21

--- quartz/cfg.ts
+++ quartz/cfg.ts
@@ -93,7 +93,6 @@ export interface QuartzConfig {
 export interface FullPageLayout {
   head: QuartzComponent
   header: QuartzComponent[]
-  beforeBody: QuartzComponent[]
   pageBody: QuartzComponent
   afterBody: QuartzComponent[]
   left: QuartzComponent[]
