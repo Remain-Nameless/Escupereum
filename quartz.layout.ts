b3bf77351e4e027d01793c4ae99ca1113e4ae777
Renmain-Nameless <evita.dragonowliss@mail.ru>
2025-12-04 19:08:07 +0300

    vault backup: 2025-12-04 19:08:07

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,6 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  left: [Navbar()],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
