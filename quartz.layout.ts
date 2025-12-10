ae3ec46d224dc6048167b62f5259514a608875e6
Renmain-Nameless <evita.dragonowliss@mail.ru>
2025-12-10 15:38:10 +0300

    vault backup: 2025-12-10 15:38:10

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,7 +45,7 @@ Component.Explorer({
   // omitted but shown later
   filterFn: (node) => {
     // set containing names of everything you want to filter out
-    const omit = new Set(["Hidden", "Ужас перед Бездной"])
+    const omit = new Set(["Hidden", "Ужас перед Бездной"]),
  
     // can also use node.slug or by anything on node.data
     // note that node.data is only present for files that exist on disk
