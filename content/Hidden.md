47b4ea1896e1945f6bcbe59bef719c267712f120
Renmain-Nameless <evita.dragonowliss@mail.ru>
2025-12-10 14:16:23 +0300

    vault backup: 2025-12-10 14:16:23

--- content/Hidden.md
+++ content/Hidden.md
@@ -1,1 +1,4 @@
+---
+explorerexclude:
+---
 мм
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -74,13 +74,7 @@ export const defaultListPageLayout: PageLayout = {
     }),
     Component.Explorer({
 	filterFn: (node) => {
-    // set containing names of everything you want to filter out
-    const omit = new Set(["Hidden", "tags", "термин"])
- 
-    // can also use node.slug or by anything on node.data
-    // note that node.data is only present for files that exist on disk
-    // (e.g. implicit folder nodes that have no associated index.md)
-    return !omit.has(node.displayName.toLowerCase())
+    return node.data?.tags?.includes("explorerexclude") !== true
   },
 }),
   ],
