35e3a95a277df9ddd0efeb51e017fdd21940fdac
Renmain-Nameless <evita.dragonowliss@mail.ru>
2025-12-10 15:24:09 +0300

    vault backup: 2025-12-10 15:24:09

--- content/Hidden/Hidden.md
+++ content/Hidden/Hidden.md
@@ -1,4 +1,4 @@
 ---
-tags:
+tags: hidden
 ---
 мм
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,12 +45,7 @@ Component.Explorer({
   // omitted but shown later
   filterFn: (node) => {
     // set containing names of everything you want to filter out
-    const omit = new Set(["content/Hidden"])
- 
-    // can also use node.slug or by anything on node.data
-    // note that node.data is only present for files that exist on disk
-    // (e.g. implicit folder nodes that have no associated index.md)
-    return !omit.has(node.displayName.toLowerCase())
+    const omit = new Set(["Hidden"])
   },
   // what order to apply functions in
   order: ["filter", "map", "sort"],
