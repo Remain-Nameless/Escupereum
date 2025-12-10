cc602a3cf5e0b9e2abe86d45a1423037b760bb61
Renmain-Nameless <evita.dragonowliss@mail.ru>
2025-12-10 22:07:35 +0300

    vault backup: 2025-12-10 22:07:34

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -46,7 +46,7 @@ const defaultOptions: Options = {
       return -1
     }
   },
-  filterFn: (node) => node.slugSegment !== ["tags"],
+  filterFn: (node) => node.slugSegment !== ["tags", "hidden"],
   order: ["filter", "map", "sort"],
 }
 
