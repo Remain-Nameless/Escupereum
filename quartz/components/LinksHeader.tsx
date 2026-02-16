b3c0e29b095213b66a64cb0ecdf8ed46eef5878c
unknown <evita.dragonowliss@mail.ru>
2026-02-16 16:32:47 +0300

    vault backup: 2026-02-16 16:32:46

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -17,7 +17,7 @@ export default (() => {
           </span>
 		  <span>
             <a href="https://remain-nameless.github.io/Escupereum/Об-авторе">
-			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="user-circle"><path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z"/><path d="M12 8v4"/><path d="M12 16h.01"/></svg>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3"/><circle cx="12" cy="10" r="3"/><circle cx="12" cy="12" r="10"/></svg>
 			Об авторе</a>
           </span>
         </div>
