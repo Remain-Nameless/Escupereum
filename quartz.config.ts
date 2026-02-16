4382aebc7aece7cf6677fad1db57eb3bdbb8ce66
Renmain-Nameless <evita.dragonowliss@mail.ru>
2026-02-16 20:28:38 +0300

    vault backup: 2026-02-16 20:28:38

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
 		title: "Spectral SC",
-        header: "Playfair Display",
+        header: "Alegreya",
         body: "EB Garamond",
         code: "Roboto",
 		textAlign: "justify",
