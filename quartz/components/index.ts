6c7b1dfb977c9c1ca78a2e32bd9d10b2b979f9a3
unknown <evita.dragonowliss@mail.ru>
2026-02-16 16:15:21 +0300

    vault backup: 2026-02-16 16:15:20

--- quartz/components/index.ts
+++ quartz/components/index.ts
@@ -21,7 +21,6 @@ import MobileOnly from "./MobileOnly"
 import RecentNotes from "./RecentNotes"
 import Breadcrumbs from "./Breadcrumbs"
 import Comments from "./Comments"
-import CustomHeader from "./CustomHeader"
 import LinksHeader from "./LinksHeader"
 import Flex from "./Flex"
 import ConditionalRender from "./ConditionalRender"
@@ -50,7 +49,6 @@ export {
   NotFound,
   Breadcrumbs,
   LinksHeader,
-  CustomHeader,
   Comments,
   Flex,
   ConditionalRender,
