71cda5bb2cfd0707f7d3f3ea9447d8176b90490d
unknown <evita.dragonowliss@mail.ru>
2026-02-17 12:08:45 +0300

    vault backup: 2026-02-17 12:08:45

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -24,6 +24,19 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
+		<p></p> 
+        <ul>
+          <li>
+            <a href="#">
+            Scroll to top ↑
+            </a> 
+          </li>
+          <li>
+            <a id="random-page-button">
+            Random Page 🎲
+            </a>
+          </li>
+        </ul>
       </footer>
     )
   }
