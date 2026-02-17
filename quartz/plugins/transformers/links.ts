abd709ee5caba47ad142abacbf05f3dcc1cc223d
unknown <evita.dragonowliss@mail.ru>
2026-02-17 13:47:12 +0300

    vault backup: 2026-02-17 13:47:11

--- quartz/plugins/transformers/links.ts
+++ quartz/plugins/transformers/links.ts
@@ -31,14 +31,7 @@ const defaultOptions: Options = {
   lazyLoad: false,
   externalLinkIcon: false,
 }
-//фильтрация ссылок на отсутствующие страницы
-const isAvailableInternalLink = (slug: SimpleSlug, allSlugs: FullSlug[]) => {
-  // if the slug is the index, it's always available
-  if(slug.endsWith("index") || slug.endsWith("index.html") || slug.endsWith("/")) return true
-  // if the slug is the tags page, it's always available
-  if(slug.startsWith("tags/")) return true
-  return allSlugs.some((s) => s.startsWith(slug))
-}
+
 export const CrawlLinks: QuartzTransformerPlugin<Partial<Options>> = (userOpts) => {
   const opts = { ...defaultOptions, ...userOpts }
   return {
@@ -131,9 +124,7 @@ export const CrawlLinks: QuartzTransformerPlugin<Partial<Options>> = (userOpts) 
                   outgoing.add(simple)
                   node.properties["data-slug"] = full
 				  
-					// Добавляет класс missing для ссылок на отсутствующие страницы
-                  if (!isAvailableInternalLink(simple, ctx.allSlugs)) classes.push("missing")
-                }
+
 
                 // rewrite link internals if prettylinks is on
                 if (
