de6846a87ac527c5295ba80bd94a56e39aac0135
unknown <evita.dragonowliss@mail.ru>
2026-02-17 10:12:56 +0300

    vault backup: 2026-02-17 10:12:53

--- quartz/components/Breadcrumbs.tsx
+++ quartz/components/Breadcrumbs.tsx
@@ -29,7 +29,7 @@ interface BreadcrumbOptions {
 }
 
 const defaultOptions: BreadcrumbOptions = {
-  spacerSymbol: "❯",
+  spacerSymbol: "•✦•",
   rootName: "Главная",
   resolveFrontmatterTitle: true,
   showCurrentPage: false,
