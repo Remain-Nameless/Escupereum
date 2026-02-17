57d7e8716d5a6da9c49914439464791ae72e0fd0
unknown <evita.dragonowliss@mail.ru>
2026-02-17 09:30:09 +0300

    vault backup: 2026-02-17 09:30:08

--- quartz/plugins/transformers/ofm.ts
+++ quartz/plugins/transformers/ofm.ts
@@ -57,7 +57,7 @@ const defaultOptions: Options = {
   enableYouTubeEmbed: true,
   enableVideoEmbed: true,
   enableCheckbox: false,
-  disableBrokenWikilinks: true,
+  disableBrokenWikilinks: false,
 }
 
 const calloutMapping = {
