1bcd243c0e597cb2fcaa3304b5675937a811ac3b
unknown <evita.dragonowliss@mail.ru>
2026-02-17 12:18:33 +0300

    vault backup: 2026-02-17 12:18:33

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -2,6 +2,7 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
+import script from "./scripts/randomPage.inline"
 
 interface Options {
   links: Record<string, string>
@@ -28,12 +29,12 @@ export default ((opts?: Options) => {
         <ul>
           <li>
             <a href="#">
-            Scroll to top ↑
+            В начало страницы ↑
             </a> 
           </li>
           <li>
             <a id="random-page-button">
-            Random Page 🎲
+            Случайная страница 🎲
             </a>
           </li>
         </ul>
