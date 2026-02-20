85248b343df51864e9bf734fe58983827dece2a2
unknown <evita.dragonowliss@mail.ru>
2026-02-20 15:23:00 +0300

    vault backup: 2026-02-20 15:22:59

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -17,7 +17,7 @@ export default (() => {
           </span>
 		  <span>
             <a href="https://remain-nameless.github.io/Escupereum/Мироустройство">
-			<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="gray" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle></svg>
+			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle></svg>
 			Мироустройство</a>
           </span>
           <span>
